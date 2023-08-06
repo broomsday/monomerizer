f37c32d3c591281dba34148f5d50c1e5ba6cfafe
Aron Broom <broomsday@gmail.com>
2023-08-05 23:11:44 -0400

    Remove unused ProteinMPNN functions

--- monomerizer/proteinmpnn.py
+++ monomerizer/proteinmpnn.py
@@ -4,20 +4,10 @@ Functions for making ProteinMPNN input or analyzing output.
 
 
 from pathlib import Path
-from typing import NamedTuple
 
 import biotite.structure as bts
 
 from monomerizer import pdb
-
-
-class Redesign(NamedTuple):
-    sequence: list[str]
-    merged_sequence: str
-    unique_chains: int
-    score: float
-    recovery: float
-    source: str
 
 
 def make_fixed_positions_jsonl(
@@ -101,63 +91,3 @@ def make_symmetric_positions_jsonl(
         )
 
     return symmetric_positions_dict
-
-
-def parse_redesign(annotation: str, sequence: str) -> Redesign:
-    """
-    From the ProteinMPNN output lines, build the Redesign data.
-    """
-    sequences = sequence.strip().split("/")
-
-    unique_sequences = []
-    for seq in sequences:
-        if seq not in unique_sequences:
-            unique_sequences.append(seq)
-    merged_sequence = "_".join(unique_sequences)
-
-    unique_chains = len(set(sequences))
-
-    score = [
-        float(info.split("=")[1])
-        for info in annotation.split(",")
-        if "score" in info.split("=")[0]
-    ][0]
-
-    if "seq_recovery" in annotation:
-        recovery = [
-            float(info.split("=")[1])
-            for info in annotation.split(",")
-            if "seq_recovery" in info.split("=")[0]
-        ][0]
-    else:
-        recovery = 1.0
-
-    if "sample" in annotation:
-        source = "design"
-    else:
-        source = "wt"
-
-    return Redesign(
-        sequence=sequences,
-        merged_sequence=merged_sequence,
-        unique_chains=unique_chains,
-        score=score,
-        recovery=recovery,
-        source=source,
-    )
-
-
-def load_proteinmpnn_output(fasta: Path) -> list[Redesign]:
-    """
-    Read a ProteinMPNN output file and return a list of designs.
-    """
-    with open(fasta, mode="r", encoding="utf-8") as fa_file:
-        lines = fa_file.readlines()
-
-    redesigns = []
-    for i in range(0, len(lines), 2):
-        redesign = parse_redesign(lines[i], lines[i + 1])
-        if redesign not in redesigns:
-            redesigns.append(parse_redesign(lines[i], lines[i + 1]))
-
-    return redesigns
